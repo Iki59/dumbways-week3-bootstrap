37c62447b91fe3ffe4fac973e9daceea5bbd660e
Iki59 <rizkiboediman59@gmail.com>
2023-05-24 00:37:33 +0700

    task day 5

--- js/my-project.js
+++ js/my-project.js
@@ -62,10 +62,53 @@ function addProject(event) {
     event.preventDefault();
 
     let projectName = document.getElementById("input-project-title").value;
-    let startDate = document.getElementById("input-project-start-date").value;
-    let endDate = document.getElementById("input-project-end-date").value;
+    let startDate = new Date(document.getElementById("input-project-start-date").value);
+    let endDate = new Date (document.getElementById("input-project-end-date").value);
     let description = document.getElementById("input-project-content").value;
     let image = document.getElementById("input-project-image").files;
+
+    // durasi
+    function getDifferenceTime() {
+    let distance = endDate - startDate; // hasilnya milidetik
+    console.log(distance);
+  
+    let milisecond = 1000; // milisecond
+    let secondInHours = 3600; // 1 jam 3600 detik
+    let hoursInDays = 24; // 1 hari 24 jam
+    let daysInMonth = 30; // 30 hari sebulan
+    let monthInYear = 12; // 12 bulan setahun
+    let distanceDay = Math.floor(
+      distance / (milisecond * secondInHours * hoursInDays)
+    ); 
+    let distanceMonth = Math.floor(
+      distance / (milisecond * secondInHours * hoursInDays * daysInMonth)
+    );
+    let distanceYear = Math.floor(
+      distance /
+        (milisecond * secondInHours * hoursInDays * daysInMonth * monthInYear)
+    );
+
+    if (distanceYear > 0) {
+        return `${distanceYear} Tahun - ${distanceMonth} Bulan - ${distanceDay} Hari`;
+      } else if (distanceMonth > 0) {
+        return `${distanceMonth} Bulan - ${distanceDay} Hari`;
+      } else if (distanceDay > 0) {
+        return `${distanceDay} Hari`
+      }
+    }
+
+    let durasi = getDifferenceTime();
+    
+    // checkbox tech
+    // const javaIcon = '<i class="fa-brands.fa-java"></i>';
+    // const javascriptIcon = '<i class="fa-brands.fa-python"></i>';
+    // const pythonIcon = '<i class="fa-brands.fa-square-js"></i>';
+    // const phpIcon = '<i class="fa-brands.fa-php"></i>';
+
+    // let checkboxJava = documents.getElementById("input-java").checked ? javaIcon: "";
+    // let checkboxJavascript = documents.getElementById("input-javascript").checked ? javascriptIcon: "";
+    // let checkboxPython = documents.getElementById("input-python").checked ? pythonIcon: "";
+    // let checkboxPhp = documents.getElementById("input-php").checked ? phpIcon: "";
 
     // untuk membuat url gambar agar tampil
     image = URL.createObjectURL(image[0]);
@@ -75,8 +118,15 @@ function addProject(event) {
         projectName,
         startDate,
         endDate,
+        durasi,
         description,
         image,
+        // checkboxJava,
+        // checkboxJavascript,
+        // checkboxPython,
+        // checkboxPhp,
+        postAt: new Date(),
+        author: "Muhammad Rizki B",
     }
 
     dataProject.push(project)
@@ -87,7 +137,6 @@ function addProject(event) {
 
 function renderProject() {
     document.getElementById("contents").innerHTML = "";
-
     for (let index = 0; index < dataProject.length; index++)
     document.getElementById("contents").innerHTML += `
 
@@ -96,21 +145,112 @@ function renderProject() {
         <img src="${dataProject[index].image}" alt="apps" />
     </div>
     <div class="project-content">
-    <div class="btn-group">
-        <button class="btn-edit">Edit</button>
-        <button class="btn-delete">Delete</button>
-    </div>
-        <h1>
-         <a href="project-detail.html" target="_blank">
-         ${dataProject[index].projectName}</a>
-        </h1>
-        <div class="detail-project-content">
-        Durasi: ${dataProject[index].startDate} | ${dataProject[index].endDate}
-         <p>
-         ${dataProject[index]. description}
+    <h1>
+    <a href="project-detail.html" target="_blank">
+    ${dataProject[index].projectName}</a>
+    </h1>
+    <div class="detail-project-content">
+    <p>Durasi: ${dataProject[index].durasi} </p>
+    <p style="font-size: 10px">${getFullTime(dataProject[index].postAt)} | ${
+            dataProject[index].author
+        } </p>
+        <p>
+        ${dataProject[index]. description}
         </p>
+        <div class="tech-icon">
+        <i class="fa-brands fa-java"></i>
+        <i class="fa-brands fa-python"></i>
+        <i class="fa-brands fa-square-js"></i>
+        <i class="fa-brands fa-php"></i>
+      </div>
+        <div class="btn-group">
+            <button class="btn-edit">Edit</button>
+            <button class="btn-delete">Delete</button>
+        </div>
+        <div style="float: right; margin: 10px">
+                <p style="font-size: 15px; color: grey">${getDistanceTime(dataProject[index].postAt)}
+                </p>
+    </div>
     </div>
     </div>
     </div>
 `
 }
+// untuk menambah interval
+// {/* <div style= "float: right;margin: 10px">
+// <p style="font-size: 10px; color: grey">${getDistanceTime(dataProject[index].postAt)}
+// </p>
+// </div> */}
+
+function getFullTime (time) {
+    // console.log ("get full time");
+    // let time = newDate();
+    // console.log(time)
+
+    let monthName = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    // console.log(monthName[5]);
+
+    let date = time.getDate();
+    let monthIndex = time.getMonth();
+    let year = time.getFullYear();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+
+    if (hours <= 9) {
+        hours = "0" + hours;
+    } else if (minutes <= 9) {
+        minutes = "0" + minutes;
+    }
+
+    return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+}
+
+function getDistanceTime(time) {
+    let timeNow = new Date();
+    let timePost = time;
+
+    // waktu sekarang
+    let distance = timeNow - timePost;  //dia hasilnya milidetik
+    console.log(distance);
+
+    let milisecond = 1000; //milisecond
+    let secondInHours = 3600 // 1 Jam itu 3600 detik
+    let hoursInDays = 24; //1 Hari itu 24 Jam
+
+    let distanceDay = Math.floor(
+        distance / (milisecond * secondInHours * hoursInDays)); //jadi ini 1/8640000
+    let distanceHours = Math.floor(
+        distance / (milisecond * 60 * 60)); //jadi 1/3600000
+    let distanceMinutes = Math.floor(
+        distance / (milisecond * 60)); //jadi 1/60000
+    let distanceSeconds = Math.floor(
+        distance / milisecond);
+    
+        if (distanceDay > 0) {
+            return `${distanceDay} Day Ago`
+        } else if (distanceHours > 0) {
+            return `${distanceHours} Hours Ago`
+        } else if (distanceMinutes > 0) {
+            return `${distanceMinutes} Minutes Ago`
+        } else if (distanceSeconds > 0) {
+            return `${distanceSeconds} Seconds Ago`
+        }
+}
+
+setInterval(function () {
+    renderProject();
+}, 10000);
+
